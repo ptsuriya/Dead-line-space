552cf8c8f2e97a26c56acd1ae9d35b62f3ff88ea
miocute <42216465+miocute@users.noreply.github.com>
2019-03-17 03:02:10 +0700

    Revert "upd"
    This reverts commit 2feb88b81f8aa56d611216eb782676a59c0784d2, reversing
    changes made to b8c029a59f7cdb7ab286645e066630f7c3882cfd.

--- local/javatheme.js
+++ local/javatheme.js
@@ -2,11 +2,7 @@
 
 
 var rocket = box('ROCKET', 180, 560, 40, 40);
-<<<<<<< HEAD
 var laser = box('LASER', -1000, 330, 10, 50);
-=======
-var laser = box('LASER', -1000, 330, 5, 50);
->>>>>>> eefe9ecdec9b183569ac8226122916a4688259c3
 var left=37, right=39, up=38, down=40, specbar=32; // Key code each button.
 var speedrocket = 5;
 var controller = new Object();
@@ -281,7 +277,7 @@ function retry(){
 	score = 0;
 	distance = 0;
 	rocket = box('ROCKET', 180, 560, 40, 40);
-	laser = box('LASER', -1000, 330, 5, 50);
+	laser = box('LASER', -1000, 330, 2, 50);
 	iterations = 0;
 	enemies = new Array();
 	document.getElementById('END').style.visibility="hidden";
